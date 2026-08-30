14407485683fa0ede60127aab90bf01ba07c1390
Courage Siameh <courage336@outlook.com>
2026-08-30 16:36:50 +0000

    feat(jobs): initialize jobs domain module exports

---
+++ packages/core/src/rag_core/jobs/__init__.py
@@ -0,0 +1,5 @@
+"""Background job domain and ports."""
+
+from rag_core.jobs.models import DispatchReceipt, JobEnvelope, JobOperation, JobStage, JobStatus
+
+__all__ = ["DispatchReceipt", "JobEnvelope", "JobOperation", "JobStage", "JobStatus"]
